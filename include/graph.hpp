e499702410e745ecbaebe30f469f5d9b82f5d5f0
corrodedHash <corrodedHash@users.noreply.github.com>
2018-01-24 03:35:53 +0100

    Generalized

--- include/graph.hpp
+++ include/graph.hpp
@@ -5,14 +5,45 @@
 #include <string>
 #include <vector>
 
-using NumberGraph = boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS, boost::property<boost::vertex_index_t, unsigned>>;
+using NumberGraph = boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS>;
 
 NumberGraph createSquareSequenceGraph(unsigned until);
 void appendSquareSequenceGraph(unsigned new_index, NumberGraph& g);
-std::optional<std::vector<boost::graph_traits<NumberGraph>::vertex_descriptor>> has_path(const NumberGraph& g);
 
 inline bool is_square_num(unsigned long value)
 {
   unsigned int root = static_cast<unsigned int>(std::sqrt(value));
   return (root * root == value);
+}
+
+template <typename T>
+std::optional<std::vector<typename boost::graph_traits<T>::vertex_descriptor>> has_path(const T& g)
+{
+  std::vector<std::pair<typename boost::graph_traits<T>::adjacency_iterator, typename boost::graph_traits<T>::adjacency_iterator>> iterator_stack;
+  std::vector<typename boost::graph_traits<T>::vertex_descriptor> visited;
+
+  visited.reserve(boost::num_vertices(g));
+  iterator_stack.reserve(boost::num_vertices(g));
+  for (auto[root, it_end] = boost::vertices(g); root != it_end; ++root) {
+    visited.push_back(*root);
+    iterator_stack.push_back(boost::adjacent_vertices(visited.back(), g));
+    while (!iterator_stack.empty()) {
+      while (iterator_stack.back().first != iterator_stack.back().second) {
+        if (std::find(std::begin(visited), std::end(visited), *iterator_stack.back().first) == std::end(visited)) {
+          visited.push_back(*iterator_stack.back().first);
+          ++iterator_stack.back().first;
+          iterator_stack.push_back(boost::adjacent_vertices(visited.back(), g));
+          break;
+        }
+        ++iterator_stack.back().first;
+      }
+      if (iterator_stack.back().first == iterator_stack.back().second) {
+        visited.pop_back();
+        iterator_stack.pop_back();
+      } else if (visited.size() == boost::num_vertices(g)) {
+        return visited;
+      }
+    }
+  }
+  return std::nullopt;
 }
--- lib/graph.cpp
+++ lib/graph.cpp
@@ -5,6 +5,7 @@
 #include <boost/graph/graph_traits.hpp>
 #include <iostream>
 
+
 void appendSquareSequenceGraph(unsigned new_index, NumberGraph& g)
 {
   boost::add_vertex(g);
@@ -19,43 +20,8 @@ NumberGraph createSquareSequenceGraph(unsigned until)
 {
   NumberGraph result;
   for (unsigned i = 0; i < until; ++i) {
-    boost::add_vertex(result);
-    for (unsigned j = 0; j < i; ++j) {
-      if (is_square_num(j + i + 2)) {
-        boost::add_edge(i, j, result);
-      }
-    }
+    appendSquareSequenceGraph(i, result);
   }
   return result;
 }
 
-std::optional<std::vector<boost::graph_traits<NumberGraph>::vertex_descriptor>> has_path(const NumberGraph& g)
-{
-  std::vector<std::pair<boost::graph_traits<NumberGraph>::adjacency_iterator, boost::graph_traits<NumberGraph>::adjacency_iterator>> iterator_stack;
-  std::vector<boost::graph_traits<NumberGraph>::vertex_descriptor> visited;
-
-  visited.reserve(boost::num_vertices(g));
-  iterator_stack.reserve(boost::num_vertices(g));
-  for (auto[root, it_end] = boost::vertices(g); root != it_end; ++root) {
-    visited.push_back(*root);
-    iterator_stack.push_back(boost::adjacent_vertices(visited.back(), g));
-    while (!iterator_stack.empty()) {
-      while (iterator_stack.back().first != iterator_stack.back().second) {
-        if (std::find(std::begin(visited), std::end(visited), *iterator_stack.back().first) == std::end(visited)) {
-          visited.push_back(*iterator_stack.back().first);
-          ++iterator_stack.back().first;
-          iterator_stack.push_back(boost::adjacent_vertices(visited.back(), g));
-          break;
-        }
-        ++iterator_stack.back().first;
-      }
-      if (iterator_stack.back().first == iterator_stack.back().second) {
-        visited.pop_back();
-        iterator_stack.pop_back();
-      } else if (visited.size() == boost::num_vertices(g)) {
-        return visited;
-      }
-    }
-  }
-  return std::nullopt;
-}
