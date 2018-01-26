fbeaa3a85af64db9ef40ea50dbfca1e589673c24
corrodedHash <corrodedHash@users.noreply.github.com>
2018-01-26 04:15:22 +0100

    Added graph printing facility

--- tools/main.cpp
+++ tools/main.cpp
@@ -1,21 +1,23 @@
 #include "graph.hpp"
+#include <boost/graph/graphviz.hpp>
 #include <boost/program_options.hpp>
 #include <cmath>
 #include <iostream>
 #include <string>
 #include <vector>
 
-int main(int argc, char** args)
+void print_graph(unsigned int size)
 {
-  namespace po = boost::program_options;
-  unsigned int start, end;
-  po::options_description desc("Allowed options");
-  desc.add_options()("start", po::value<unsigned int>(&start)->default_value(1), "value to start searching at")("end", po::value<unsigned int>(&end)->default_value(0), "stop building sequences at this value");
+  NumberGraph test = createSquareSequenceGraph(static_cast<unsigned int>(size));
+  auto printer = [](std::ostream& out, const auto& v) {
+    out << "[label=\"" << v + 1 << "\"]";
+  };
+  boost::write_graphviz(std::cout, test, printer);
+}
 
-  po::variables_map vm;
-  po::store(po::command_line_parser(argc, args).options(desc).run(), vm);
-  po::notify(vm);
-  NumberGraph test = createSquareSequenceGraph(static_cast<unsigned int>(start));
+void iterate_graph(unsigned int start, unsigned int end)
+{
+  NumberGraph test = createSquareSequenceGraph(start);
   for (unsigned int i = start; i != end; ++i) {
     auto a = has_path(test);
     if (a) {
@@ -29,6 +31,23 @@ int main(int argc, char** args)
     }
     std::cout << "\n";
     appendSquareSequenceGraph(test);
+  }
+}
+int main(int argc, char** args)
+{
+  namespace po = boost::program_options;
+  unsigned int start, end;
+  po::options_description desc("Allowed options");
+  desc.add_options()("start", po::value<unsigned int>(&start)->default_value(1), "value to start searching at")("end", po::value<unsigned int>(&end)->default_value(0), "stop building sequences at this value")("print", "only print graph");
+
+  po::variables_map vm;
+  po::store(po::command_line_parser(argc, args).options(desc).run(), vm);
+  po::notify(vm);
+
+  if (vm.count("print")) {
+    print_graph(start);
+  } else {
+    iterate_graph(start, end);
   }
   return 0;
 }
