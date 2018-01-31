9c778b7c73ef6d12a0a9b821ccd26d899ec67c7d
corrodedHash <corrodedHash@users.noreply.github.com>
2018-01-31 16:51:25 +0100

    Added benchmark

--- benchmark/whole.cpp
+++ benchmark/whole.cpp
@@ -1,32 +1,44 @@
 #include "graph.hpp"
 #include <benchmark/benchmark.h>
 
-static void BM_Whole(benchmark::State& state)
-{
+static void BM_Whole(benchmark::State& state) {
   for (auto _ : state) {
     (void)_;
-    auto test = createSquareSequenceGraph<NumberGraph>(static_cast<unsigned int>(state.range(0)));
+    auto test = createSquareSequenceGraph<NumberGraph>(
+        static_cast<unsigned int>(state.range(0)));
     HamiltonPathFinder<NumberGraph> finder(test);
     auto x = finder.next();
   }
 }
-static void BM_GraphGen(benchmark::State& state)
-{
+static void BM_GraphGen(benchmark::State& state) {
   for (auto _ : state) {
     (void)_;
-    createSquareSequenceGraph<NumberGraph>(static_cast<unsigned int>(state.range(0)));
+    createSquareSequenceGraph<NumberGraph>(
+        static_cast<unsigned int>(state.range(0)));
   }
 }
-static void BM_PathFindStandard(benchmark::State& state)
-{
-  auto test = createSquareSequenceGraph<NumberGraph>(static_cast<unsigned int>(state.range(0)));
+static void BM_PathFindStandard(benchmark::State& state) {
+  auto test = createSquareSequenceGraph<NumberGraph>(
+      static_cast<unsigned int>(state.range(0)));
   for (auto _ : state) {
     (void)_;
     HamiltonPathFinder<NumberGraph> finder(test);
-    benchmark::DoNotOptimize(finder.next());
+    finder.next();
+  }
+}
+static void BM_PathFindExhaustive(benchmark::State& state) {
+  auto test = createSquareSequenceGraph<NumberGraph>(
+      static_cast<unsigned int>(state.range(0)));
+  for (auto _ : state) {
+    (void)_;
+    HamiltonPathFinder<NumberGraph> finder(test);
+    for (const auto& x : finder) {
+      (void)x;
+    }
   }
 }
 
 BENCHMARK(BM_Whole)->RangeMultiplier(2)->Range(1, 32);
 BENCHMARK(BM_GraphGen)->RangeMultiplier(2)->Range(1, 32);
 BENCHMARK(BM_PathFindStandard)->RangeMultiplier(2)->Range(1, 32);
+BENCHMARK(BM_PathFindExhaustive)->RangeMultiplier(2)->Range(1, 32);
